7fad07cef4e26858807c764901764725f4790967
jootiee <purrrtov@ya.ru>
2022-04-30 10:16:28 +0700

    init

---
+++ bot.py
@@ -0,0 +1,41 @@
+import telebot
+from telebot import types
+from main import *
+
+TOKEN = "1761664191:AAF2O_JDOnrv4D4uY9UT3Kflefo5jfEGS_g"
+bot = telebot.TeleBot(TOKEN)
+
+element = None
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, """Привет. Я - бот, строящий график полураспада некоторых (многих) элементов.
+Для того чтобы начать, напиши порядковый номер элемента.""")
+
+@bot.message_handler(commands=['list'])
+def send_list(message):
+	bot.send_message(message.chat.id, "Список элементов:\n" + "\n".join([f"{elem['name']} ({elem['symbol']}) - {elem['number']}" for elem in elements]))
+
+@bot.message_handler(func=lambda m: True)
+def elem_faq(message):
+    global element
+    if message.text not in numbers:
+        bot.send_message(message.chat.id, "Элемент не найден. Проверь правильность ввода или попробуй другой.")
+    else:
+        element = Element(message.text)
+        bot.reply_to(message, element)
+        bot.send_message(message.chat.id, "Введи любую точку...")
+        bot.register_next_step_handler(message, send_graph)
+
+def send_graph(message):
+    point = eval(message.text) 
+    if point < 0:
+        bot.send_message(message.chat.id, "Произошла ошибка. Попробуй еще раз.")
+        bot.register_next_step_handler(message, send_graph)
+    
+    plotter = Plotter(eval(message.text), element())
+    img = plotter.create_plot()
+    bot.send_photo(message.chat.id, open(element.symbol + message.text + ".png", 'rb'))
+
+ 
+bot.polling()
---
+++ main.py
@@ -0,0 +1,93 @@
+import matplotlib.pyplot as plt
+from io import StringIO
+import base64
+import json
+
+
+class Plotter:
+    def __init__(self, point, element):
+        self.point = point
+        self.element = element
+        self.period = eval(" ".join(self.element["T"].split()[:-1]))
+
+    def get_dimensions(self):
+        middle = self.get_middle()
+        if middle:
+            if middle - self.period >= 0:
+                if middle - self.period * 2 >= 0:
+                    return [middle - self.period * x for x in range(2, -3, -1)]
+                return [middle - self.period * x for x in range(1, -4, -1)]
+            return [middle + self.period * x for x in range(5)][::-1]
+        return [self.period * x for x in range(5)]
+
+    def get_middle(self):
+        if self.point < 0:
+            return 0
+        
+        bottom = abs(self.point // self.period * self.period - self.point)
+        top = round(self.point / self.period) * self.period - \
+            self.point if self.point >= self.period else self.period - self.point
+        min_distance = min(bottom, top)
+        if min_distance == bottom:
+            return self.point - bottom
+        return self.point + top
+
+    def create_plot(self):
+        plt.switch_backend('Agg')
+
+        self.x = self.get_dimensions()
+        self.y = [1 / (2 ** (point // self.period))
+                  if point else 1 for point in self.x]
+
+        fig, ax = plt.subplots()
+
+        plt.scatter(self.point, 1 / (2 ** (self.point / self.period)),
+                    color='orange', s=40, marker='o')
+
+        if self.element["dimension"] == "years":
+            plt.xlabel("t, лет")
+        if self.element["dimension"] == "minutes":
+            plt.xlabel("t, минут")
+        if self.element["dimension"] == "seconds":
+            plt.xlabel("t, секунд")
+        if self.element["dimension"] == "days":
+            plt.xlabel("t, дней")
+        if self.element["dimension"] == "hours":
+            plt.xlabel("t, часов")
+        plt.ylabel("Доля атомов, N₀")
+
+        plt.xlim(self.x[0], self.x[-1])
+
+        plt.plot(self.x, self.y)
+
+        plt.savefig(self.element["symbol"] + str(self.point) + '.png')
+
+
+class Element:
+    def __init__(self, number):
+        self.number = number
+        
+        for element in elements:
+            if element["number"] == self.number:
+                self.elem = element
+                self.mass_number = element["mass number"]
+                self.name = element["name"]
+                self.symbol = element["symbol"]
+                self.period = element["T"]
+                self.radiation = element["radiation"]
+                break
+
+    def __repr__(self):
+        return f"""Элемент {self.name} ({self.symbol}):
+Порядковый номер: {self.number}
+Массовое число изотопа: {self.mass_number}
+Период полураспада: {self.period}
+Излучение: {self.radiation}
+"""
+
+    def __call__(self):
+        return self.elem
+
+
+numbers = ['89', '95', '85', '7', '4', '97', '83', '83', '83', '1', '26', '77', '53', '19', '98', '48', '27', '96', '99', '100', '15', '87', '9', '9', '103', '101', '11', '11', '93', '102', '91', '61', '84', '84', '94', '94', '94', '94', '88', '86', '37', '37', '16', '43', '90', '92', '92', '92', '92', '92', '6', '55']
+elements = json.load(open("elements.json", encoding="utf-8"))
